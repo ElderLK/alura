1d2bd0a37e4ab65b7c79b819322dfde3f2ff107e
ElderWiser <elder.kuss@wisereducacao.com>
2019-08-25 19:29:52 -0300

    add cross-env and plugings to webpack

--- react/Webpack/client/webpack.config.js
+++ react/Webpack/client/webpack.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const babilPlugin = require('babili-webpack-plugin');
+
+let plugins = [];
+
+if(process.env.NODE_ENV == 'production'){
+    plugins.push(new babilPlugin());
+}
 
 module.exports = {
     entry: './app-src/app.js',
@@ -16,5 +23,6 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins
 }
